0ede6d88c3cd2d70fcba9155fd2babb995d6debe
Jabal531 <jabalscript@gmail.com>
2026-02-02 13:53:57 +0700

    Update site configuration details

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
+  website: "https://it-s-just-a-hunch-but-i-think-he-s.vercel.app/", // replace this with your deployed domain
+  author: "ci i o",
   profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  desc: "It's just a hunch, but I think he’s lying",
+  title: "It's just a hunch",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
